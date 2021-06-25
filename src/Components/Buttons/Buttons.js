4a4dd1768229bab76b20c533db43bac5eb631949
Rahmonjon22 <rahmonjon@yahoo.com>
2021-06-25 16:22:43 +0200

    adding buttons

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -2,11 +2,35 @@
 import React from 'react';
 import '../Buttons/Buttons.css';
 
-function Buttons () {
-  return(
-    <div className="Buttons-page">
-      <h1>this is a button</h1>
-    </div>
-  )
-}
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+
+const SIZES = ['btn--medium', 'btn--large'];
+
+const Buttons = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <Link to='/sign-up' className='btn-mobile'>
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
 export default Buttons;
