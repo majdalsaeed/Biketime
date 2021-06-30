a946d4c8ba13666d5de0fe7bbf703482cd4acdb1
Moe <notreal@email.com>
2021-06-30 14:31:30 +0200

    nada

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { DataProvider } from "./DataProvider";
 import Products from "./Products";
-import Basket from "./Basket";
 import "./Shop.css";
 
 function Shop() {
